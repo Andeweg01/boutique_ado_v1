3dd06a5393b917c654febedb0152c76b48c2348d
Sascha Andeweg <sascha@designed.irish>
2020-07-23 14:06:37 +0000

    added checkout

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout'
+
+    # other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -55,6 +58,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'boutique_ado.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -70,8 +75,13 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
