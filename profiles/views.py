4cff0503954c1c91e6248e852e26760bf13ca568
Sascha Andeweg <sascha@designed.irish>
2020-08-01 14:11:36 +0000

    corrected arguments on line 33 of views.py in profiles app

--- profiles/views.py
+++ profiles/views.py
@@ -30,11 +30,11 @@ def profile(request):
     return render(request, template, context)
 
 
-def order_history(request, template, context):
+def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
 
     messages.info(request, (
-        f'This is a past confirmations for order number {order_number}.'
+        f'This is a past confirmation for order number {order_number}. '
         'A confirmation email was sent on the order date.'
     ))
 
