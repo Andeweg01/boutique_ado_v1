f54be7ffea98295f6cac4fe245c06e4680756f92
Sascha Andeweg <sascha@designed.irish>
2020-08-03 18:41:17 +0000

    added emailconfirmation in settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -207,7 +207,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'designed.irish@gmail.com'
+    DEFAULT_FROM_EMAIL = 'designed.irish@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
