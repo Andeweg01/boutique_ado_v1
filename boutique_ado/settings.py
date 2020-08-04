13bc122b89e47d686ea255023eb54671c9b6966d
Sascha Andeweg <sascha@designed.irish>
2020-08-04 19:21:09 +0000

    debug false in settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = ['sascha-boutique-ado.herokuapp.com', 'localhost']
 
