909605f1776f56f0ef6baf8eaff6923f925c3ca8
Sascha Andeweg <sascha@designed.irish>
2020-08-04 19:25:55 +0000

    reversed DEBUG line in settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['sascha-boutique-ado.herokuapp.com', 'localhost']
 
