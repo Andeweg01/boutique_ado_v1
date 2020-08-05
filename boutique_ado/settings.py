a5303ba5ed6c5ba758f89bcdec335bf92c230e13
Sascha Andeweg <sascha@designed.irish>
2020-08-05 17:14:29 +0000

    changed USE_S3 back to USE_AWS

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -172,7 +172,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-if 'USE_S3' in os.environ:
+if 'USE_AWS' in os.environ:
     # Cache control
     AWS_S3_OBJECT_PARAMETERS = {
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
