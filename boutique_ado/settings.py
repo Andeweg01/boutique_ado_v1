62fb35ac4b6f5b0503dc0dfb676a3e1b70ca5be7
Sascha Andeweg <sascha@designed.irish>
2020-08-05 14:59:30 +0000

    changes in STATIC_URL and pathname

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -165,9 +165,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
