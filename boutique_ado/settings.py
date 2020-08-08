5b03a9f2e84ea9feb052d61cd6a8cc15d970ae9d
Sascha Andeweg <sascha@designed.irish>
2020-08-08 11:32:56 +0000

    last attempt to make it work with AWS

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -162,7 +162,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaScript, images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
